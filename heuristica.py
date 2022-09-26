90d774e6f06dff5ee934fa7d1462a6fc5b0e4fb7
Marco Chitolina <68135699+MarcoChit0@users.noreply.github.com>
2022-09-26 13:59:26 -0300

    heuristic.py: ok

--- heuristica.py
+++ heuristica.py
@@ -9,16 +9,16 @@
     2.2.SELECIONA(2) uma nova população P entre P, P'.
 """
 
+from asyncore import write
 from copy import deepcopy
 from math import ceil, floor
+from statistics import *
+import time
 from numpy import *
 
-COUNT = 0
-NUM_EXECUCOES = 20
-
-def increment():
-    global COUNT
-    COUNT = COUNT + 1
+TIME_LIMIT = 18000  # 5 horas 
+NUM_INSTANCIAS = 2  # 2 execuções
+NUM_CONTEINERS = 10 # fixo -- de acordo com o enunciado
 
 # ok
 def gera_solucoes(num_itens, num_conteiners, num_solucoes, pesos_conteiners, pesos_itens):
@@ -30,7 +30,6 @@ def gera_solucoes(num_itens, num_conteiners, num_solucoes, pesos_conteiners, pes
 
 def atribui_itens_aleatoriamente(num_itens, num_conteiners, pesos_conteiners, pesos_itens):
     atribuicao_itens = [[0 for i in range(num_itens)] for k in range(num_conteiners)]
-    print(atribuicao_itens)
     for conteiner in range(num_conteiners):
         for item in range(num_itens):
             if posso_atribuir(atribuicao_itens, item, conteiner, num_itens, num_conteiners, pesos_itens, pesos_conteiners) and (random.choice([0,1], size=1, p=[0.5,0.5])[0] == 1):
@@ -40,9 +39,11 @@ def atribui_itens_aleatoriamente(num_itens, num_conteiners, pesos_conteiners, pe
 
 def posso_atribuir(atribuicao, item, conteiner, num_itens, num_conteiners, pesos_itens, pesos_conteiners):
     sum = 0
+    # verifica se eu já selecionei o item
     for cont in range(num_conteiners):
         sum += atribuicao[cont][item]
     
+    # verifica se tem espaço no conteiner para adicionar o item
     if sum == 0:
         peso_atual = 0
         for i in range(num_itens):
@@ -54,19 +55,15 @@ def posso_atribuir(atribuicao, item, conteiner, num_itens, num_conteiners, pesos
     else:
         return False
 
-def criterio_parada_satisfeito():
-    increment()
-    if COUNT < NUM_EXECUCOES:
-        return False
-    else:
-        return True
+def criterio_parada_satisfeito(time_start, time_duration):
+    return not (time.time() < time_start + time_duration)
 
 def torneio(solucoes, num_participantes_torneio, valores_itens, valores_pares_itens, num_itens, num_conteiners):
     indice_solucoes_participantes = random.choice(len(solucoes), size=num_participantes_torneio)
     participantes = []
     for index in indice_solucoes_participantes:
         participantes.append(deepcopy(solucoes[index]))
-    melhores_participantes = sorted(participantes, key=lambda p: funcao_objetivo(p, valores_itens, valores_pares_itens, num_itens, num_conteiners))
+    melhores_participantes = sorted(participantes, key=lambda p:funcao_objetivo(p, valores_itens, valores_pares_itens, num_itens, num_conteiners))
     return melhores_participantes
 
 def recombinacao(solucao_01, solucao_02, num_particao=1):
@@ -107,9 +104,22 @@ def mutacao(solucao, num_conteiners, num_itens, pesos_itens, pesos_conteiners):
                 solucao[conteiner][item_aleatorio] = 1
 
 
-# TODO
-def busca_local(solucao):
-    pass
+
+def busca_local(solucao, num_itens, num_conteiners, pesos_itens, pesos_conteiners):
+    for item in range(num_itens):
+        if not foi_selecionado(solucao, item, num_conteiners):
+            for conteiner in range(num_conteiners):
+                if posso_atribuir(solucao, item, conteiner, num_itens, num_conteiners, pesos_itens, pesos_conteiners):
+                    solucao[conteiner][item] = 1
+                    break
+
+def foi_selecionado(solucao, item, num_conteiners):
+    for conteiner in range(num_conteiners):
+            if solucao[conteiner][item] == 1:
+                return True
+    return False
+
+
 
 def selecao(populacao_original, nova_populacao, valores_itens, valores_pares_itens, num_itens, num_conteiners, num_solucoes_populacao_original, alpha=0.1, beta=0.9):
     # alpha = orinal + nova
@@ -155,24 +165,108 @@ def heuristica(
     # gama,
     ):
     populacao_original = gera_solucoes(num_itens, num_conteiners, num_solucoes_populacao_original, pesos_conteiners, pesos_itens)
-    while not criterio_parada_satisfeito():
-        #  TODO: gerar pop nova baseada na antiga
+    time_duration = TIME_LIMIT
+    time_start = time.time()
+    while not criterio_parada_satisfeito(time_start, time_duration):
         nova_populacao =  []
         while len(nova_populacao) < len(populacao_original):
-            sol_01 = torneio(populacao_original, num_participantes_torneio, valores_itens, valores_pares_itens, num_itens, num_conteiners)[0]
-            sol_02 = torneio(populacao_original, num_participantes_torneio, valores_itens, valores_pares_itens, num_itens, num_conteiners)[0]
+            lista_solucoes = torneio(populacao_original, num_participantes_torneio, valores_itens, valores_pares_itens, num_itens, num_conteiners)
+            sol_01 = lista_solucoes[0]
+            lista_solucoes = torneio(populacao_original, num_participantes_torneio, valores_itens, valores_pares_itens, num_itens, num_conteiners)
+            sol_02 = lista_solucoes[0]
             recombinacao(sol_01, sol_02, num_particao)
             mutacao(sol_01, num_conteiners, num_itens, pesos_itens, pesos_conteiners)
             mutacao(sol_02, num_conteiners, num_itens, pesos_itens, pesos_conteiners)
             # melhoria:
-            # valor_sol_01 = busca_local(sol_01)
-            # valor_sol_02 = busca_local(sol_02)
+            busca_local(sol_01, num_itens, num_conteiners, pesos_itens, pesos_conteiners)
+            busca_local(sol_02, num_itens, num_conteiners, pesos_itens, pesos_conteiners)
             nova_populacao.append(sol_01)
             nova_populacao.append(sol_02)
         populacao_original = selecao(populacao_original, nova_populacao, valores_itens, valores_pares_itens, num_itens, num_conteiners, num_solucoes_populacao_original, alpha, beta)
-    return populacao_original
-print(heuristica(4, 2, 5, [3, 3], [1,2,3,4], 3, 2, [1,2,3,4],[[0,4,1,1],[4,0,1,1],[1,1,0,1],[1,1,1,0]]))
-# print(funcao_objetivo([[1,1,0,0],[0,0,1,0]], [1,2,3,4], [[0,4,1,1],[4,0,1,1],[1,1,0,1],[1,1,1,0]], 4, 2))
+    melhores_individuos = sorted(populacao_original, key=lambda individuo: funcao_objetivo(individuo, valores_itens, valores_pares_itens, num_itens, num_conteiners), reverse=True)
+    return melhores_individuos
+# print(heuristica(num_itens= 4,  num_conteiners=2, num_solucoes_populacao_original=5, pesos_conteiners=[3, 3], pesos_itens=[1,2,3,4], num_participantes_torneio= 2, valores_itens=[1,2,3,4],valores_pares_itens=[[0,4,1,1],[4,0,1,1],[1,1,0,1],[1,1,1,0]]))
 
-# TODO: para cada uma das 10 instâncias do problema dos contêiners: rodar o algoritmo 5 vezes, salvando os resultados, e realizar a média. 
-# OBS: para cada vez que eu for rodar o algoritmo, rodar para a mesma semente aleatória, isto é, seed in [1,2,3,4,5].
+def read_file(file_name, num_conteiners=NUM_CONTEINERS):
+    """
+        Objetivo: ler o arquivo de instâncias e salvar os dados do problema.
+        file_name: string --> nome do arquivo de entrada.
+    """
+    input_dir = "./in/"
+    f = open(input_dir + file_name)
+    itens = 0
+    counter = 0
+    valor_itens = []
+    dic_valor_par_itens = {}
+    volume_itens = []
+    for line in f.readlines():
+        if counter == 0:
+            # linha 0 = número de itens
+            itens = int(line) 
+        elif counter == 1: 
+            # linha 1 = valor dos itens
+            valor_itens = [int(s)  for s in line.split(" ") if s != "\n"]
+        elif 2 <= counter <= itens: 
+            # linhas [2, número de itens] = valor dos pares de itens
+            dic_valor_par_itens[counter-2] = [int(s)  for s in line.split(" ") if s != "\n"]
+        elif counter == itens + 4:
+            # linha número de itens + 4 = volume dos itens
+            volume_itens = [int(s)  for s in line.split(" ") if s != "\n"]
+        counter += 1
+    f.close()
+
+    # transformar o dicinário de valores de pares de itens (obtido das linhas [2, número de itens]) em uma matriz simétrica de valores
+    coluna = 1
+    valor_par_itens = [[0 for i in range(itens)] for j in range(itens)]
+    for key in dic_valor_par_itens:
+        it_coluna = deepcopy(coluna)
+        linha = deepcopy(key)
+        for valor in dic_valor_par_itens[key]:
+            if it_coluna == linha:
+                valor_par_itens[linha][it_coluna] = 0
+            else:
+                valor_par_itens[linha][it_coluna] = valor
+                valor_par_itens[it_coluna][linha] = valor
+            it_coluna += 1
+        coluna += 1
+    
+    # volume dos contêiners de acordo com o enunciado do problema
+    volume_conteiners = [floor((0.8/num_conteiners) * sum(volume_itens)) for k in range(num_conteiners)]
+    return itens, valor_itens, valor_par_itens, volume_itens, volume_conteiners
+
+
+def main():
+    variacoes = [1,2,3,4,5]
+    for n in range(NUM_INSTANCIAS):
+        # recupera os dados dos arquivos de entrada
+        file_name = "pcmcdc" + str(n+1) + ".txt"
+        itens, valores, pares, volumes, conteiners = read_file(file_name)
+        # para cada uma das alterações no parâmetro: rodar a heuristica, salvar dados. Por fim, realizar a média.
+        melhores_individuos = {}    # dicionário de listas.
+        for variacao in variacoes:
+            random.seed(seed=variacao)                      # [1, 2, ..., 5]
+            num_solucoes_populacao_original = 50*variacao   # [50, 100,..., 250]
+            num_participantes_torneio = 5*variacao          # [5, 10, ..., 25]
+            num_particao = random.choice(NUM_CONTEINERS-1, size=1)[0]
+            mais_bem_adaptados = heuristica(num_itens=itens,num_conteiners=NUM_CONTEINERS,valores_itens=valores, valores_pares_itens=pares, pesos_itens=volumes, pesos_conteiners=conteiners, num_solucoes_populacao_original=num_solucoes_populacao_original, num_participantes_torneio=num_participantes_torneio, num_particao=num_particao)
+            melhores_individuos[variacao] = mais_bem_adaptados
+        write_file(melhores_individuos=melhores_individuos, file_name=file_name, valores_pares_itens=pares, num_itens=itens, valores_itens=valores)
+
+
+def write_file(melhores_individuos, file_name, valores_pares_itens, num_itens, valores_itens):
+    out_dir = "./heuristic/"
+    out_pref = "out_heuristic_"
+    file = open(out_dir + out_pref + file_name, "w")
+    for variacao in melhores_individuos:
+        valores = [funcao_objetivo(ind, valores_itens=valores_itens, valores_pares_itens=valores_pares_itens, num_itens=num_itens, num_conteiners=NUM_CONTEINERS) for ind in melhores_individuos[variacao]]
+        file.write("variação: "+str(variacao)+"\n")
+        file.write("melhor valor: "+str(valores[0])+"\n")
+        file.write("média: "+str(mean(valores))+"\n")
+        file.write("mediana: "+str(median(valores))+"\n")
+        file.write("moda: "+str(mode(valores))+"\n")
+        file.write("desvio padrão: "+str(stdev(valores))+"\n")
+        file.write("variância: "+str(pvariance(valores))+"\n")
+        file.write("----------------------------------\n")
+    file.close()    
+ 
+main()        
